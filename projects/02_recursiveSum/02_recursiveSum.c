0800d004296ce80c1ad4078e5a3541e1595e96e3
LewisCollum <lewiscollum4@gmail.com>
2019-11-18 14:04:38 -0500

    Added 'stack' section to project 2.

--- projects/02_recursiveSum/02_recursiveSum.c
+++ projects/02_recursiveSum/02_recursiveSum.c
@@ -1,20 +1,34 @@
+/*
+  EE 466 Computer Architecture
+  Fall 2019
+  Instructor: Dr. Chen Liu
+  Project 2: Recursive Calls
+  Name: Lewis Collum
+  Student ID: 0621539
+  Major: EE & CE
+  Email Address: colluml@clarkson.edu
+  Date of the report: November 19, 2019
+*/
+
 #include <stdio.h>
 
 extern long long int mysum(long long int n);
 
 int main() {
-	int willContinue = 1;
-	while(willContinue) {
-		int n;
-		printf("Input (n): ");
-		scanf("%d", &n);
+  int willContinue = 1;
+  while(willContinue) {
+    // get user input for parameter n.
+    int n;
+    printf("Input (n): ");
+    scanf("%d", &n);
 
-		if (n >= 1) {
-			printf("%d\nContinue? (1/0): ", mysum(n));
-			scanf("%d", &willContinue);
-		} else {
-			printf("n must be larger than or equal to 1.\n");
-		}
-	}
-	return 0;
+    if (n >= 1) {
+      // print result. Then ask if user wants to continue.
+      printf("%d\nContinue? (1/0): ", mysum(n));
+      scanf("%d", &willContinue);
+    } else {
+      printf("n must be larger than or equal to 1.\n");
+    }
+  }
+  return 0;
 }
